8e26ffb648390995a8184586addd38e81a187692
Michał Siwek <mjzsiwek@gmail.com>
2023-01-16 23:36:32 +0100

    Partly done bridge simulation

--- bridgerectifier/bridge_rectifier.py
+++ bridgerectifier/bridge_rectifier.py
@@ -1,13 +1,60 @@
-# import matplotlib.pyplot as plt
+import os
+import numpy as np
+import matplotlib.pyplot as plot
+
 
 class BridgeRectifier:
-    def a(self):
-        print("bridge rectifier")
+    TARGET_RIPPLE_VALUE = 1
 
-# x = [1, 3, 5, 7]
-# y = [2, 4, 6, 1]
-# plt.plot(x, y)
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
-# plt.title("A simple line graph")
-# plt.show()
+    def simulate_bridge(self):
+        # Showing scheme image
+        self.__show_image()
+
+        # Asking user for input
+        self.__load_data_from_user()
+
+        # Calculating capacitor value
+        capacitor_value = self.__calculate_capacitor_value()
+        print("Value of the capacitor (C1) is: " + str(capacitor_value))
+
+        # Create Transient Analysis chart
+        self.__show_transient_analysis_chart()
+
+    @staticmethod
+    def __show_image():
+        path = os.getcwd()
+        os.startfile(path + "/bridgerectifier/schema.png")
+
+    def __load_data_from_user(self):
+        print("\n\nPlease provide following data:")
+        self.input_voltage = float(input("Input voltage (V): "))
+        self.frequency = float(input("Frequency (Hz): "))
+        self.resistance = float(input("Resistance (Ohm): "))
+
+    def __calculate_capacitor_value(self):
+        return (self.input_voltage * self.TARGET_RIPPLE_VALUE) / (2 * self.frequency * self.resistance)
+
+    def __show_transient_analysis_chart(self):
+        plot.title("Transient Analysis")
+        plot.ylabel("Voltage")
+        plot.xlabel("Time")
+
+        x_axis = self.__create_chart_x_axis()
+        y_axis = self.__create_chart_y_axis(x_axis)
+
+        plot.plot(x_axis, y_axis)
+        plot.grid(True, which = 'both')
+        plot.axhline(y = self.input_voltage, color = 'k')
+        plot.show()
+
+    def __create_chart_x_axis(self):
+        return np.arange(0, self.__calculate_arange_end_value(), self.__calculate_arange_step())
+
+    def __calculate_arange_end_value(self):
+        return 1 / self.frequency * 3
+
+    def __calculate_arange_step(self):
+        return 1 / self.frequency / 10
+
+    def __create_chart_y_axis(self, x_axis):
+        return np.sin(x_axis)
--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from voltagedivider.voltage_divider import VoltageDivider
 
 def exercise_1():
     voltage_divider = VoltageDivider()
-    voltage_divider.a()
+    voltage_divider.voltage_divider()
+
+def jump_to_next_exercise():
+    print("Do you want to go to another exercise?\n\"y\" for yes, to exit any other key")
+    if (input() != "y"):
+        exit()
 
 
 def exercise_2():
     bridge_rectifier = BridgeRectifier()
-    bridge_rectifier.a()
+    bridge_rectifier.simulate_bridge()
 
 
 if __name__ == '__main__':
-    exercise_1()
+    #exercise_1()
+    jump_to_next_exercise()
     exercise_2()
